c31b015ba9861c18dba1f8b2aa8500c24773a4d1
NimrodBar <131991393+NimiB2@users.noreply.github.com>
2025-04-20 20:31:37 +0300

    save local changes before rebase

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flasgger import Swagger
 from mongo_db_connection_manager import MongoConnectionManager
 from controller.ad_entrypoints import ad_routes_blueprint
-from routes import init_routes
+from controller.ad_entrypoints import ad_routes_blueprint
 import os
+# from routes import init_routes
+
 
 
 app = Flask(__name__)
@@ -12,8 +14,10 @@ Swagger(app)
 # Initalize Database Connection
 MongoConnectionManager.init_db()
 
-#Importe the routes
-init_routes(app)
+app.register_blueprint(ad_routes_blueprint)
+
+# #Importe the routes
+# init_routes(app)
 
 
 if __name__ == '__main__':
